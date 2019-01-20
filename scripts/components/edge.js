bd77a61803d14b6a9b02d6bb5324d680c17b71ce
Jed <darkjedi9922@gmail.com>
2019-01-20 15:34:59 +0200

    Изменяет вращение каждого элемента в ребре на вращение всего компонента целиком.

--- scripts/components/edge.js
+++ scripts/components/edge.js
@@ -4,32 +4,34 @@ class Edge extends React.Component {
     }
 
     render() {
+        // Чтобы не вращать каждый элемент по отдельности, просто берем все элементы
+        // в состоянии 0-го градуса и вращаем все целиком в <g>.
+
         const x1 = this.props.start.x;
         const y1 = this.props.start.y;
-        const x2 = this.props.end.x;
-        const y2 = this.props.end.y;
+
+        // SVG rotate вращает не в ту сторону, потому значение с минусом.
+        const degree = -this.calcDegree();
+
+        // Если при рисовании в состоянии 0-го градуса взять обычный x2, длина
+        // ребра будет изменяться в зависимости от угла. Поэтому берем константый
+        // x1 с константной длинной.
+        const planeX2 = x1 + this.calcLength();
 
         let arrowElement = null;
         if (this.props.arrow) {
-            // SVG rotate вращает не в ту сторону (или оно идет не туда, из-за
-            // расположения точки, относительно которой вращаем), потому значение с 
-            // минусом.
-            const arrowDegree = -this.calcDegree();
-
-            const arrowRotateCenterX = x2;
-            const arrowRotateCenterY = y2;
-
             arrowElement = <polygon points={
-                (x2 - 25) + ',' + y2 + ' ' +
-                (x2 - 35) + ',' + (y2 - 5) + ' ' +
-                (x2 - 35) + ',' + (y2 + 5)}
-                transform={"rotate(" + arrowDegree + " " +
-                    arrowRotateCenterX + " " + arrowRotateCenterY + ")"} />
+                (planeX2 - 25) + ',' + y1 + ' ' +
+                (planeX2 - 35) + ',' + (y1 - 5) + ' ' +
+                (planeX2 - 35) + ',' + (y1 + 5)} />
         }
 
         return (
-            <g>
-                <line x1={x1} y1={y1} x2={x2} y2={y2} stroke="black" />
+            <g transform={"rotate(" + degree + " " + x1 + " " + y1 + ")"} >
+                <path fill="none" d={
+                    'M ' + x1 + ',' + y1 + ' ' + 
+                    planeX2 + ',' + y1} 
+                    stroke="black" />
                 {arrowElement}
             </g>
         );
@@ -42,7 +44,7 @@ class Edge extends React.Component {
         const y2 = this.props.end.y;
 
         // Найдем угол через угол между вектором и его проекцией на ось Х.
-        const edgeLength = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        const edgeLength = this.calcLength();
         const projectionLength = x2 - x1;
 
         // Градусов в радианах.
@@ -54,5 +56,14 @@ class Edge extends React.Component {
         // В 3 и 4 четвертях косинус не расчитывает больше 180. Нужно это исправить.
         if (y2 > y1) return 180 + 180 - result;
         return result;
+    }
+
+    calcLength() {
+        const x1 = this.props.start.x;
+        const y1 = this.props.start.y;
+        const x2 = this.props.end.x;
+        const y2 = this.props.end.y;
+        
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 }
