80a99edd89d03e546ea55b8149fc081b85590f19
Jed <darkjedi9922@gmail.com>
2019-01-25 17:05:46 +0200

    Setup build electron tool.

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ var app = require('electron').app;
 var BrowserWindow = require('electron').BrowserWindow;
 var path = require('path');
 
+// Далее создаётся ссылка на объект Window. Это делается для того, чтобы окно не 
+// закрывалось автоматически, когда объект будет обработан сборщиком мусора.
 var mainWindow = null;
 
+// При закрытии всех окон приложения следует из него выйти. В OS X это событие 
+// является общим для приложений и их баров меню, поэтому здесь присутствует условие, 
+// отбрасывающее эту платформу.
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit();
